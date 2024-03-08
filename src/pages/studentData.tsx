e947aa93589dbed51d48f0ef3e76ae46dbd9b250
ShikshaFinder <janiharsh794@gmail.com>
2024-03-08 18:42:02 +0530

    student list

---
+++ src/pages/studentData.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Profile from "../components/profile"
+
+function studentData() {
+
+
+
+
+    
+  return (
+    <>
+    <Profile
+    Board='CBSE'
+    Standard='12th'
+    name='Rahul'
+    Medium='English'
+    city='Delhi'
+    email='janiharsh794@gmail.com'
+    state='Delhi'
+   
+    />
+
+    </>
+  )
+}
+
+export default studentData
