27306bdfac66848a7771ea4bf3d9c20abb40772f
ShikshaFinder <janiharsh794@gmail.com>
2024-03-12 11:55:56 +0530

    signout added

---
+++ src/pages/signout.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import supabase from "../../supabase";
+import { useToast } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
+
+function signout() {
+  const toast = useToast();
+
+  const handleSignOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      toast({
+        title: "Error.",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success.",
+        description: "You have been signed out",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
+
+  return (
+    <div>
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Button onClick={handleSignOut} colorScheme="teal">
+          Sign Out
+        </Button>
+      </Stack>
+    </div>
+  );
+}
+
+export default signout;
