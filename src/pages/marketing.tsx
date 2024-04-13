59dc5cfe8122228af98fd462065db4866087fc6a
ShikshaFinder <janiharsh794@gmail.com>
2024-04-13 17:56:39 +0530

    marketing for industry"

--- src/pages/marketing.tsx
+++ src/pages/marketing.tsx
@@ -1,7 +1,6 @@
-"use client";
 import { ReactElement } from "react";
 import { Box, SimpleGrid, Icon, Text, Stack, Flex } from "@chakra-ui/react";
-import { FcAssistant, FcDonate, FcInTransit } from "react-icons/fc";
+import { FcAdvertising, FcBullish, FcMoneyTransfer } from "react-icons/fc";
 import Pricing from "../components/pricing"
 
 interface FeatureProps {
@@ -37,21 +36,21 @@ export default function SimpleThreeColumns() {
       <Box p={4}>
         <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
           <Feature
-            icon={<Icon as={FcAssistant} w={10} h={10} />}
+            icon={<Icon as={FcAdvertising} w={10} h={10} />}
             title={"Targeted ADS"}
             text={
               "We can help you find the perfect students for your school. We will promote your school to promising students who meet your requirements and live in your area."
             }
           />
           <Feature
-            icon={<Icon as={FcDonate} w={10} h={10} />}
+            icon={<Icon as={FcBullish} w={10} h={10} />}
             title={"Best call to action"}
             text={
               "After watching your ADS, students can directly feel the admission form of your school."
             }
           />
           <Feature
-            icon={<Icon as={FcInTransit} w={10} h={10} />}
+            icon={<Icon as={FcMoneyTransfer} w={10} h={10} />}
             title={"Value for Money"}
             text={
               "You will be able to make an impression on the minds of parents and students who are finding the schools and actively looking out for admission"
@@ -59,8 +58,8 @@ export default function SimpleThreeColumns() {
           />
         </SimpleGrid>
       </Box>
-      
-      <Pricing/>
+
+      <Pricing />
     </>
   );
 }
