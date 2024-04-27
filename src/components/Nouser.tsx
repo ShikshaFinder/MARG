bd4df8d0d4a98d705ce70e353a3c0140b8171187
ShikshaFinder <janiharsh794@gmail.com>
2024-04-27 22:33:58 +0530

    signup with google removed

--- src/components/Nouser.tsx
+++ src/components/Nouser.tsx
@@ -1,13 +1,18 @@
+"use client";
 import React from "react";
 import { Button, Stack } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import { Alert, AlertIcon } from "@chakra-ui/react";
 
 function Nouser() {
   const Router = useRouter();
 
-  
   return (
     <>
+      <Alert status="warning">
+        <AlertIcon />
+        If you have signed up please reload,or it will be done in few seconds.
+      </Alert>
       <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
         <Button
           colorScheme="teal"
@@ -26,6 +31,17 @@ function Nouser() {
           }}
         >
           SignIn
+        </Button>
+        <Button
+          colorScheme="teal"
+          display={"flex"}
+          onClick={() => {
+            if (typeof window !== "undefined") {
+              window.location.reload();
+            }
+          }}
+        >
+          Reload Page
         </Button>
       </Stack>
     </>
