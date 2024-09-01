a5a88d8930b3fa4aebd3ab5448a3e2a599da1817
ShikshaFinder <janiharsh794@gmail.com>
2024-09-01 12:49:03 +0530

    drop down state added

--- src/pages/addsignal.tsx
+++ src/pages/addsignal.tsx
@@ -1,20 +1,34 @@
-import React from 'react'
-import AddCard from "../components/cardAdd"
-import Card from "../components/card"   
+import React from "react";
+import AddCard from "../components/cardAdd";
+import Card from "../components/card";
 import { Grid } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
-import supabase from '../../supabase';
-import { Toast } from "@chakra-ui/react"; 
+import supabase from "../../supabase";
+import { Toast, FormControl, FormLabel, Select } from "@chakra-ui/react";
+import { useForm, Controller } from "react-hook-form";
+import { state } from "@/components/state";
+
+
+
+interface State {
+  districts: string[];
+  state: string;
+}
 
 function addsignal() {
-      const [userData, setUserData] = useState<any[] | null>(null);
-
+  const form = useForm();
+  const { register, handleSubmit, control, watch } = form;
+  const selectedState = watch("State");
+  const [userData, setUserData] = useState<any[] | null>(null);
+  
+  const [states, setStates] = useState<State[]>(state.states);
+  const districts =
+  states.find((state) => state.state === selectedState)?.districts || [];
   async function getInfo() {
     try {
       let { data, error } = await supabase
         .from("information")
-        .select("address")
-       
+        .select("address");
 
       setUserData(data); // Append new data
       // setLoading(false);
@@ -37,10 +51,47 @@ function addsignal() {
 
   return (
     <>
- <Grid
-          templateColumns={{ base: "repeat(2, 1fr)", lg: "repeat(4, 1fr)" }}
-          gap={1}
-        >        {userData &&
+      <Grid
+        templateColumns={{ base: "repeat(2, 1fr)", lg: "repeat(4, 1fr)" }}
+        gap={1}
+      >
+        <FormControl isRequired>
+          <FormLabel>State</FormLabel>
+          <Select
+            {...register("State", { required: true })}
+            name="State"
+            placeholder="Select State"
+          >
+            {states.map((stateObj) => (
+              <option key={stateObj.state} value={stateObj.state}>
+                {stateObj.state}
+              </option>
+            ))}
+          </Select>
+        </FormControl>
+        <br />
+        <FormControl isRequired>
+          <FormLabel>District/city</FormLabel>
+          <Select
+            {...register("city", { required: true })}
+            name="city"
+            placeholder="Select District"
+          >
+            {districts.map((district) => (
+              <option key={district} value={district}>
+                {district}
+              </option>
+            ))}
+          </Select>
+        </FormControl>{" "}
+      </Grid>
+      <br />
+      <Grid
+        templateColumns={{ base: "repeat(2, 1fr)", lg: "repeat(4, 1fr)" }}
+        gap={1}
+      >
+        {" "}
+        {userData &&
           userData.map(
             (
               information: {
@@ -50,11 +101,10 @@ function addsignal() {
             ) => <Card key={index} address={information.address} />
           )}
         {/* </Grid> */}
-
         <AddCard />
       </Grid>
     </>
   );
 }
 
-export default addsignal
+export default addsignal;
